8893d98190081df5c3471418a0f8520c007904bb
prakashraj1181994 <prakashraj1181994@gmail.com>
2025-04-20 16:20:25 +0530

    Update test.c
    removed hello

--- test.c
+++ test.c
@@ -7,7 +7,10 @@ int main()
     printf("prakash raj...!\n");
 	printf("obito..!\n");
 	printf("Rin..!\n");
+<<<<<<< HEAD
 	printf("naruto..!\n");
 	printf("hello..!\n");
+=======
+>>>>>>> e2e3443 (Update test.c)
 	return 0;
 }
