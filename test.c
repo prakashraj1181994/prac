fc0678a9e53c934bf77ab9f822bd4257370a35d8
prakashraj1181994 <prakashraj1181994@gmail.com>
2025-04-20 16:09:21 +0530

    print nart added

--- test.c
+++ test.c
@@ -4,8 +4,9 @@ int main()
 	int a=15;
 	int b=10;
 	printf("hello World..!\n");
-        printf("prakash raj...!\n");
+    printf("prakash raj...!\n");
 	printf("obito..!\n");
 	printf("Rin..!\n");
+	printf("naruto..!\n");
 	return 0;
 }
