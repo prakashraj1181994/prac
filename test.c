2010efd05d5b8727af8ed7578c2d03fccab85fd3
prakashraj1181994 <prakashraj1181994@gmail.com>
2025-04-20 16:42:39 +0530

    merge conflict

--- test.c
+++ test.c
@@ -7,10 +7,7 @@ int main()
     printf("prakash raj...!\n");
 	printf("obito..!\n");
 	printf("Rin..!\n");
-<<<<<<< HEAD
 	printf("naruto..!\n");
 	printf("hello..!\n");
-=======
->>>>>>> e2e3443 (Update test.c)
 	return 0;
 }
